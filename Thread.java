01bd1742fc3d4976370c1160d8daac847c19df75
Jung111 <75231563+Jung111@users.noreply.github.com>
2022-12-29 16:17:43 +0900

    Add files via upload

---
+++ Thread.java
@@ -0,0 +1,20 @@
+
+class gugu {
+	public static void main(String[] args) {
+
+		Thread t = new Thread();
+		t.start();
+		for (int i = 0; i < args.length; i++) {
+
+		}
+
+	}
+
+}
+
+class gogo extends Thread {
+	public void run() {
+
+	}
+}
+
---
+++ Thread2.java
@@ -0,0 +1,29 @@
+class Thread2 {
+	public static void main(String[] args) {
+		Thread1_1 t1 = new Thread1_1();
+		Runnable r = new Thead1_2();
+		Thread t2 = new Thread(r);
+
+		t1.start();
+		t2.start();
+
+	}
+}
+
+class Thread1_1 extends Thread {
+	public void run() {
+		for (int i = 0; i < 5; i++) {
+			System.out.println(getName());
+
+		}
+	}
+}
+
+class Thead1_2 implements Runnable{
+	
+	public void run() {
+		for (int i = 0; i < 5; i++) {
+			System.out.println(Thread.currentThread().getName());
+		}
+	}
+}
