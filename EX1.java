735ff25056563a9f03acfd834845b25f60cebe49
Jung111 <75231563+Jung111@users.noreply.github.com>
2023-01-05 10:20:42 +0900

    Add files via upload

---
+++ EX1.java
@@ -0,0 +1,77 @@
+
+public class EX1 {
+	public static void main(String[] args) {
+
+//int형 변수 x가 10보다 크고 20보다 작을떄 true인 조건식
+//		int x = 15;
+//		if (x>10 && x<20) {
+//		  System.out.println(true);
+//		}
+
+//char 형 변수 ch 가 공백이거나 탭이 아닐때 true 인 조건식 
+		
+//		 char ch = 'Apple' ;
+//		  if (ch==' '&& ch!='\t' ) { 
+//		  System.out.println(true); }
+//		 
+		
+
+//
+//		int a = 0;
+//		for (int i = 0; i < 10; i++) {
+//			for (int j = 0; j < 5; j++) {
+//				a = i+j;
+//			}
+//		
+//		}
+//		System.out.println(a);
+
+//연산 결과는??
+//		int x = 2;
+//		int y = 5;
+//		char c = 'A';
+//		
+//		System.out.println(y<=5 || x<0 && x>2);
+//		System.out.println(y+=10 - x++);
+//		System.out.println(x+=2);
+//		System.out.println(!('A' <=c && c<='Z'));
+//		System.out.println('C'-c);
+//		System.out.println('5'-'0');
+//		System.out.println(c+1);
+//		System.out.println(++c);
+//		System.out.println(c++);
+//		System.out.println(c);
+	
+//사과: 123개 한개의 바구니에는 사과 10개 씩 넣을수있다 바구니의 갯수는?
+//		int numOfApples =123;
+//		int sizeOfBucket = 10;
+//		int numOfBucket= numOfApples / sizeOfBucket;
+//				 
+//		++numOfBucket;
+//		System.out.println(numOfBucket);
+		
+// num 값에 따라 양,음 ,0 출력하는데 삼항 연산자를 이용하기
+//		int num =10;
+//	
+//		
+//		System.out.println(num==10?"양수":-num);
+
+//num 값중 100 이하의 값은 버리는 코드 값이 456 이면 400이되고 111이면 100이 되게 하라
+//		int num = 456;
+//		System.out.println(num /400 *400);
+//num 값중 1의 자리를 바꾸는 코드이다 값이 330 이면 331 이되고 557이면 558이 되게 하라
+//		int num = 333;
+//		System.out.println(++num);
+
+//num 의 값이 24라면 6을 결과로 얻어야한다 
+		
+//	int num = 24;
+//	num = num- 30;// 음수로 나옴  답은 양수 로 나와야함
+//	System.out.println(num== 6? num : -num);
+//		
+		
+//섭씨, 화씨 변환 문제  변환 공식 'c = 5/9 x(f -32)',결과값은 소수점 셋째짜리에서 반올림
+		int fahrenheit = 100;
+			
+	}
+}
